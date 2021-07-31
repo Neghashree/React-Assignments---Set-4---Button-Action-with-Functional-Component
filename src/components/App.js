b7734d7c895b1707ebfadfeb946f41e55a159a8c
Neghashree <86755686+Neghashree@users.noreply.github.com>
2021-07-31 17:45:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,16 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 
-   let btn = () => {
-     <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-   }
+   
 function App() {
-  return (
+   
+   let [button, setButton]=React.useState()
+
+let btn = () => {
+  setButton(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+}
+   
+   return (
     <div id="main">
       // Do not alter the main div
 
