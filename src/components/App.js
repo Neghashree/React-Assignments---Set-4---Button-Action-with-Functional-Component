6426b20c00e633ee93a48b9405eab3c953138716
Neghashree <86755686+Neghashree@users.noreply.github.com>
2021-07-31 17:23:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+
+   let btn = () => {
+     <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+   }
 function App() {
   return (
     <div id="main">
       // Do not alter the main div
-   let btn = () => {<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+
     <button id="click" onClick={btn}> click</button>
     </div>
   );
